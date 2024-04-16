83ab6b3278d0e7f784bc5f1d7fda0e9efe955f7d
ritu <ritu.webol.co.uk>
2024-04-16 23:11:08 +0530

    change

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  rules: {
+    // Add your custom rules here
+  },
+};
