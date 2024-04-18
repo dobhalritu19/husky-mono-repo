efd9fcc9690d9bcdccbf296de0523c421bc813b6
ritu <ritu.webol.co.uk>
2024-04-18 18:04:50 +0530

    changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,22 @@
-import js from "@eslint/js";
+// import js from "@eslint/js";
 
-export default [
-  js.configs.recommended,
+// export default [
+//   js.configs.recommended,
 
-  {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      semi: ["error", "always"],
-    },
+//   {
+//     rules: {
+//       "no-unused-vars": "warn",
+//       "no-undef": "warn",
+//       semi: ["error", "always"],
+//     },
+//   },
+// ];
+
+module.exports = {
+  // Add your project-specific rules here
+  rules: {
+    "no-unused-vars": "warn",
+    "no-undef": "warn",
+    semi: ["error", "always"],
   },
-];
+};
