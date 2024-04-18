955ac933a91c002dfa7a113616b174ecc782d15e
ritu <ritu.webol.co.uk>
2024-04-18 19:11:14 +0530

    chnage

--- eslint.config.js
+++ eslint.config.js
@@ -15,11 +15,18 @@
 // eslint.config.js
 
 import js from "@eslint/js";
+import typescriptParser from "@typescript-eslint/parser";
 
 export default [
   js.configs.recommended,
   {
+    files: ["**/*.js", "**/*.mjs"],
+    languageOptions: {
+      parser: typescriptParser,
+    },
     rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
       semi: ["warn", "always"],
     },
     // ...other config
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const foo = "bar"; // Missing semi-colon
-const foo1 = "bar"; // Missing semi-colon
-const foo3 = "bar"; // Missing semi-colon
+const foo1 = "bar";
 
 console.log(foo);
