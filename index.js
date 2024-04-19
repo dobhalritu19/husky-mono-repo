1bbbd3d8edb4bf8b37eec53a8b687acf1dec3622
ritu <ritu.webol.co.uk>
2024-04-19 11:44:46 +0530

    change

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const foo = "bar"
-const foo1 = "bar"
-const foo2 = "bar"
+const foo = "bar";
+
 console.log(foo);
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-const foo4 = "bar"
-const foo5 = "bar"
-const foo6 = "bar"
-const foo7 = "bar"
-console.log(foo4);
+const foo1 = "bar"
+
+
+
+
+console.log(foo1);
