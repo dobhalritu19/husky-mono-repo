5ce1ad68ef666addecbe5624a419740902893cf7
ritu <ritu.webol.co.uk>
2024-04-19 10:34:39 +0530

    change

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import js from "@eslint/js";
+import eslintplugin from "@typescript-eslint/eslint-plugin";
+import eslintparser from "@typescript-eslint/parser";
 
 export default [
     js.configs.recommended,
-
     {
+        languageOptions: {
+            parser: eslintparser,
+            ecmaVersion: 5,
+            sourceType: "script"
+        },
+        "plugins": [eslintplugin],
         rules: {
             "no-unused-vars": "warn",
             "no-undef": "warn",
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const foo = 'bar';
-console.log(foo);
+var favoriteFruits = ["apple", "strawberry", "orange"];
+function addFruit(fruit) {
+    favoriteFruits.push(fruit);
+}
