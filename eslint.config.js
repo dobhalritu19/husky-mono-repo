d6c00b3facd82091d75d1a7b79a4a0143b1109af
ritu <ritu.webol.co.uk>
2024-04-19 11:27:34 +0530

    change

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,30 @@
 import js from "@eslint/js";
-
+import eslintplugin from "@typescript-eslint/eslint-plugin";
+import eslintparser from "@typescript-eslint/parser";
+import globals from "globals";
 export default [
-  js.configs.recommended,
-
-  {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      "semi": ["error", "always"],
-    },
-  },
+    js.configs.recommended,
+    {
+        files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: eslintparser,
+            ecmaVersion: 2022,
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                myCustomGlobal: "readonly"
+            }
+        },
+        plugins: {
+            eslintplugin: eslintplugin
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn",
+            "semi": ["error", "always"],
+            "eslintplugin/no-unused-vars": "error",
+            // to enforce using type for object type definitions, can be type or interface 
+            "eslintplugin/consistent-type-definitions": ["error", "type"],
+        }
+    }
 ];
