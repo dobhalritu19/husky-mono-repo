ea7398816f4de9d2fc7e1892e5e18337c9ebf2f9
ritu <ritu.webol.co.uk>
2024-04-19 09:51:42 +0530

    changes

--- test.ts
+++ test.ts
@@ -1,4 +1,10 @@
-const foo = "bar";
-const foo1 = "bar"
-const foo2 = "bar";
-console.log(foo);
+// const foo = "bar";
+// const foo1 = "bar"
+// const foo2 = "bar";
+// console.log(foo);
+
+function greet(name: string) {
+    console.log(`Hello, ${name}!`);
+}
+
+greet("World");
